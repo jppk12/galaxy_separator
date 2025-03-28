34a8dc9905f8bb33bbebed64a41acdf9d3f76262
Jahang <jaggumanfred12@gmail.com>
2025-03-28 22:39:51 +1100

    Adding WHAN diagram to the pool

--- Optical_lines.py
+++ Optical_lines.py
@@ -22,6 +22,14 @@ if module_path not in sys.path:
     
 from lum_functions import *
 
+plt.rcParams["xtick.top"] = True
+plt.rcParams["xtick.bottom"] = True
+plt.rcParams["ytick.left"] = True
+plt.rcParams["ytick.right"] = True
+plt.rcParams["axes.spines.top"] = True
+plt.rcParams["axes.spines.right"] = True
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
 #------------------#
 # Some functions
 #------------------#
@@ -51,9 +59,7 @@ def kewley(x):
 def kauffmann(x):
     return 0.61/(x-0.05)+1.3
 
-def classify(gem_1):
-    #x = np.linspace(-5,0.4,len(gem_1))
-    #x_kauf=np.linspace(-5,0,len(gem_1))
+def classify(gem_1,ha_ew='HA_EW',n2_ew = 'NIIR_EW'):
         
     k_grp = np.zeros(len(gem_1))
     k_grp[(gem_1.logo3hb>kauffmann(gem_1.logn2ha))&(gem_1.logo3hb<kewley(gem_1.logn2ha))] = 4
@@ -72,6 +78,15 @@ def classify(gem_1):
     
     
     gem_1['Blue_index'] = blue_index
+    
+    whan_index = np.zeros(len(gem_1))
+    whan_index[(gem_1.logn2ha<-0.4)&(gem_1[ha_ew]>3)] = 1 #psf
+    whan_index[(gem_1.logn2ha>-0.4)&(gem_1[ha_ew]>3)&(gem_1[ha_ew]<=6)] = 2 #wagn
+    whan_index[(gem_1.logn2ha>-0.4)&(gem_1[ha_ew]>6)] = 3#sagn
+    whan_index[gem_1[ha_ew]<=3] = 0 #fagn
+    whan_index[(gem_1[ha_ew]<0.5)&(gem_1[n2_ew]<0.5)] = 0 #passive
+    
+    gem_1['WHAN_index'] = whan_index
     return gem_1
         
         
@@ -117,8 +132,8 @@ def plot_bpt(gem_1):
     plt.legend(fontsize=12,markerscale=1.2,frameon=True)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-    plt.xlabel(r'$\log_{10}([NII]/H\alpha)$', fontsize=13)
-    plt.ylabel(r'$\log_{10}([OIII]/H\beta)$', fontsize=13)
+    plt.xlabel(r'$\rm\log_{10}([NII]/H\alpha)$', fontsize=13)
+    plt.ylabel(r'$\rm\log_{10}([OIII]/H\beta)$', fontsize=13)
     plt.xlim(-2,1.2)
     plt.ylim(-1.8,1.6)
     plt.tight_layout()
@@ -154,22 +169,45 @@ def plot_blue(gem_1):
     plt.legend(fontsize=12,markerscale=1.2,frameon=True)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-    plt.xlabel(r'$\log_{10}([NII]/H\alpha)$', fontsize=13)
-    plt.ylabel(r'$\log_{10}([OIII]/H\beta)$', fontsize=13)
+    plt.xlabel(r'$\rm\log_{10}([NII]/H\alpha)$', fontsize=13)
+    plt.ylabel(r'$\rm\log_{10}([OIII]/H\beta)$', fontsize=13)
     plt.xlim(-2,2)
     plt.ylim(-1.8,1.6)
     plt.tight_layout()
     #plt.savefig('/Users/jahang/OneDrive - Macquarie University/Luminosity Functions/Paper_data/BPT_z.pdf',
     #            format='pdf',bbox_inches='tight')
     return
+    
+def whan_plot(gem_1):
+    
+    sns.scatterplot(x=gem_1.logn2ha[gem_1.WHAN_index==3], y=gem_1.HA_EW[gem_1.WHAN_index==3],marker='^',label='sAGN',s=40, edgecolor = 'black')
+    
+    sns.scatterplot(x=gem_1.logn2ha[gem_1.WHAN_index==2], y=gem_1.HA_EW[gem_1.WHAN_index==2],marker='^',label='wAGN',s=40, edgecolor = 'black')
+    
+    sns.scatterplot(x=gem_1.logn2ha[gem_1.WHAN_index==1], y=gem_1.HA_EW[gem_1.WHAN_index==1],marker='*', color = 'purple',label='SFG',s=40, edgecolor = 'black')
+    
+    sns.scatterplot(x=gem_1.logn2ha[gem_1.WHAN_index==0], y=gem_1.HA_EW[gem_1.WHAN_index==0],marker='*', color = 'magenta',label='Retired & Passive',s=90, edgecolor = 'black')
+
+    
+    plt.plot([-0.4,-0.4],[3,1e3],linestyle='--', color='black')
+    plt.plot([-0.4,2],[6,6],linestyle='--', color='black')
+    plt.axhline(3,linestyle='--', color='black')
+    
+    plt.xlim(-6,2)
+    plt.ylim(1e-2,1e3)
+    plt.yscale('log')
+    
+    plt.xlabel(r'$\rm\log_{10}([NII]/H\alpha)$', fontsize=13)
+    plt.ylabel(r'$\rm\,EW_{H_\alpha}$', fontsize=13)
+    return
 #--------------------------------------#
 # Load the data with spectral info
 #--------------------------------------#
 
-file = '/Users/jahang/Downloads/gal_line_dr7_v5_2.csv'
-data = pd.read_csv(file)
-data['HA_EW'] = data.H_ALPHA_FLUX/data.H_ALPHA_CONT
-data['HB_EW'] = data.H_BETA_FLUX/data.H_BETA_CONT
+#file = '/Users/jahang/Downloads/gal_line_dr7_v5_2.csv'
+#data = pd.read_csv(file)
+#data['HA_EW'] = data.H_ALPHA_FLUX/data.H_ALPHA_CONT
+#data['HB_EW'] = data.H_BETA_FLUX/data.H_BETA_CONT
 
 g09_path = '/Users/jahang/Documents/PhD/AGN Catalogue/Xmatched/G09/GAMA/G09_catwise_speclines.csv'
 g23_path = '/Users/jahang/Documents/PhD/AGN Catalogue/Xmatched/G23/GAMA/G23_catwise_speclines.csv'
@@ -185,25 +223,26 @@ g09 = emission_corr(g09,n2r='NIIR_FLUX',o2r='OIIR_FLUX',o2b='OIIB_FLUX',o3r='OII
 g23 = emission_corr(g23,n2r='NIIR_FLUX',o2r='OIIR_FLUX',o2b='OIIB_FLUX',o3r='OIIIR_FLUX',
                     o3b='OIIIB_FLUX',ha='HA_FLUX',ha_ew='HA_EW',hb='HB_FLUX',hb_ew='HB_EW')
 
-data = emission_corr(data,n2r='NII_6548_FLUX',o3r='OIII_5007_FLUX',o2b='OII_3726_FLUX',o2r='OII_3729_FLUX',
-                     ha='H_ALPHA_FLUX', ha_ew='HA_EW',hb='H_BETA_FLUX',hb_ew='HB_EW',o3b='OIII_5007_FLUX')
-
-data.OIII_corr = data.OIII_corr/2
-data.logo3hb = np.log10(data.OIII_corr/data.HB_corr)
-    
-
-g09 = g09[((g09.HA_FLUX/g09.HA_FLUX_ERR)>=5)&((g09.HB_FLUX/g09.HB_FLUX_ERR)>=5)&
-          ((g09.NIIR_FLUX/g09.NIIR_FLUX_ERR)>=5)&((g09.NIIB_FLUX/g09.NIIB_FLUX_ERR)>=5)&
-          ((g09.OIIR_FLUX/g09.OIIR_FLUX_ERR)>=5)&((g09.OIIB_FLUX/g09.OIIB_FLUX_ERR)>=5)&
-          ((g09.OIIIR_FLUX/g09.OIIIR_FLUX_ERR)>=5)&((g09.OIIIB_FLUX/g09.OIIIB_FLUX_ERR)>=5)]
-
-g23 = g23[((g23.HA_FLUX/g23.HA_FLUX_ERR)>=5)&((g23.HB_FLUX/g23.HB_FLUX_ERR)>=5)&
-          ((g23.NIIR_FLUX/g23.NIIR_FLUX_ERR)>=5)&((g23.NIIB_FLUX/g23.NIIB_FLUX_ERR)>=5)&
-          ((g23.OIIR_FLUX/g23.OIIR_FLUX_ERR)>=5)&((g23.OIIB_FLUX/g23.OIIB_FLUX_ERR)>=5)&
-          ((g23.OIIIR_FLUX/g23.OIIIR_FLUX_ERR)>=5)&((g23.OIIIB_FLUX/g23.OIIIB_FLUX_ERR)>=5)]
-
 g09 = classify(g09)
 g23 = classify(g23)
+
+# data = emission_corr(data,n2r='NII_6548_FLUX',o3r='OIII_5007_FLUX',o2b='OII_3726_FLUX',o2r='OII_3729_FLUX',
+#                      ha='H_ALPHA_FLUX', ha_ew='HA_EW',hb='H_BETA_FLUX',hb_ew='HB_EW',o3b='OIII_5007_FLUX')
+#
+# data.OIII_corr = data.OIII_corr/2
+# data.logo3hb = np.log10(data.OIII_corr/data.HB_corr)
+#
+#
+# n=-999999
+# g09 = g09[((g09.HA_FLUX/g09.HA_FLUX_ERR)>=n)&((g09.HB_FLUX/g09.HB_FLUX_ERR)>=n)&
+#          ((g09.NIIR_FLUX/g09.NIIR_FLUX_ERR)>=n)&((g09.NIIB_FLUX/g09.NIIB_FLUX_ERR)>=n)&
+#          ((g09.OIIR_FLUX/g09.OIIR_FLUX_ERR)>=n)&((g09.OIIB_FLUX/g09.OIIB_FLUX_ERR)>=n)&
+#          ((g09.OIIIR_FLUX/g09.OIIIR_FLUX_ERR)>=n)&((g09.OIIIB_FLUX/g09.OIIIB_FLUX_ERR)>=n)]
+#
+# g23 = g23[((g23.HA_FLUX/g23.HA_FLUX_ERR)>=n)&((g23.HB_FLUX/g23.HB_FLUX_ERR)>=n)&
+#          ((g23.NIIR_FLUX/g23.NIIR_FLUX_ERR)>=n)&((g23.NIIB_FLUX/g23.NIIB_FLUX_ERR)>=n)&
+#          ((g23.OIIR_FLUX/g23.OIIR_FLUX_ERR)>=n)&((g23.OIIB_FLUX/g23.OIIB_FLUX_ERR)>=n)&
+#          ((g23.OIIIR_FLUX/g23.OIIIR_FLUX_ERR)>=n)&((g23.OIIIB_FLUX/g23.OIIIB_FLUX_ERR)>=n)]
 #----------------------#
 # GAMA
 #----------------------#
@@ -217,38 +256,76 @@ plt.title('G23')
 plt.show(block=False)
 plt.pause(2)
 plt.close()
+#---------------#
+# Classified data
+#---------------#
+cols = ['island_id', 'component_id', 'component_name','uberID','CATAID', 'ra_catwise',
+       'dec_catwise', 'ra_hms_cont', 'dec_dms_cont', 'ra_deg_cont',
+       'dec_deg_cont','BPT_index']
+collated_g09 = g09[cols]
 
-plt.figure(figsize=(10,6))    
-
-plt.subplot(121)
-plot_blue(g09)
-plt.subplot(122)
-plot_blue(g23)
-plt.show(block=False)
-plt.pause(2)
-plt.close()
+cols = ['Source_Name', 'uberID','CATAID','RA', 'DEC','BPT_index']
+collated_g23 = g23[cols]
 
 #-------------#
 # SDSS
 #-------------#
-data=data[((data.NII_6548_FLUX/data.NII_6548_FLUX_ERR)>=5)&
-    ((data.OIII_5007_FLUX/data.OIII_5007_FLUX_ERR)>=5)&
-    ((data.OII_3726_FLUX/data.OII_3726_FLUX_ERR)>=5)&
-    ((data.OII_3729_FLUX/data.OII_3729_FLUX_ERR)>=5)&
-    ((data.H_ALPHA_FLUX/data.H_ALPHA_FLUX_ERR)>=5)&
-    ((data.H_BETA_FLUX/data.H_BETA_FLUX_ERR)>=5)]
-    
-data = classify(data)
-data1 = data[data.BPT_index==3]
-plt.figure(figsize=(12,7))
+# data=data[((data.NII_6548_FLUX/data.NII_6548_FLUX_ERR)>=5)&
+#     ((data.OIII_5007_FLUX/data.OIII_5007_FLUX_ERR)>=5)&
+#     ((data.OII_3726_FLUX/data.OII_3726_FLUX_ERR)>=5)&
+#     ((data.OII_3729_FLUX/data.OII_3729_FLUX_ERR)>=5)&
+#     ((data.H_ALPHA_FLUX/data.H_ALPHA_FLUX_ERR)>=5)&
+#     ((data.H_BETA_FLUX/data.H_BETA_FLUX_ERR)>=5)]
+#
+# data = classify(data)
+# data1 = data[data.BPT_index==3]
+# plt.figure(figsize=(12,7))
+# plt.subplot(121)
+# plot_bpt(data)
+# plt.xlim(-2.5,1.5)
+#
+# plt.subplot(122)
+# plot_blue(data)
+# sns.kdeplot(x=data.logo2hb, y=data.logo3hb, hue=data.BPT_index, palette=["C0", "C1", "C2"])
+#
+# plt.show()
+# plt.pause(2)
+# plt.close()
+
+# Blue classification on G09 and G23 sources without Halpha line
+#----------------------#
+# Blue diagram
+#----------------------#
+plt.figure(figsize=(10,6))    
+
 plt.subplot(121)
-plot_bpt(data)
-plt.xlim(-2.5,1.5)
-
-plt.subplot(122)    
-plot_blue(data)
-sns.kdeplot(x=data.logo2hb, y=data.logo3hb, hue=data.BPT_index, palette=["C0", "C1", "C2"])
-
-plt.show()
+plot_blue(g09)
+plt.title('G09')
+plt.subplot(122)
+plot_blue(g23)
+plt.title('G23')
+plt.show(block=False)
 plt.pause(2)
 plt.close()
+
+collated_g09 = pd.merge(collated_g09,g09[['component_id','Blue_index']],on='component_id')
+collated_g23 = pd.merge(collated_g23,g23[['Source_Name','Blue_index']],on='Source_Name')
+
+
+#-----------------#
+# WHAN Diagram
+#-----------------#
+plt.figure(figsize=(10,6))    
+
+plt.subplot(121)
+whan_plot(g09)
+plt.title('G09')
+plt.subplot(122)
+whan_plot(g23)
+plt.title('G23')
+plt.show(block=False)
+plt.pause(2)
+plt.close()
+
+collated_g09 = pd.merge(collated_g09,g09[['component_id','WHAN_index']],on='component_id')
+collated_g23 = pd.merge(collated_g23,g23[['Source_Name','WHAN_index']],on='Source_Name')
