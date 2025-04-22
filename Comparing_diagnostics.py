b5c1ac9f1464556eb219a0ead8d23be4b9adca6c
Jahang <jaggumanfred12@gmail.com>
2025-04-23 07:50:46 +1000

    Adding UpSet plot

--- Comparing_diagnostics.py
+++ Comparing_diagnostics.py
@@ -13,6 +13,8 @@ import astropy.units as u
 from scipy import stats
 import seaborn as sns
 import matplotlib.gridspec as gridspec
+from upsetplot import UpSet
+from upsetplot import from_memberships
 pd.pandas.set_option('display.max_columns', None)
 import os, sys
 
@@ -34,6 +36,16 @@ plt.rcParams["axes.spines.right"] = True
 plt.rcParams["xtick.direction"] = "in"
 plt.rcParams["ytick.direction"] = "in"
 
+
+def fix_col(gem_1):
+    names = []
+    for i in range(len(gem_1)):
+        names.insert(i,gem_1.component_id.iloc[i].split(' ')[-1])
+    
+    gem_1.component_id = names
+
+    return gem_1
+    
 def list_files_in_directory(folder_path):
   """Lists all files in the given directory.
 
@@ -53,6 +65,7 @@ def list_files_in_directory(folder_path):
 
 files = list_files_in_directory(path_2)
 
+os.chdir(path_2)
 #-------------------#
 # Reading the files
 #-------------------#
@@ -66,6 +79,7 @@ g09_Xray = pd.concat([g09_main,g09_hard])
 
 # Optical
 g09_opt = pd.read_csv(files[-1])
+g09_opt = fix_col(g09_opt)
 g23_opt = pd.read_csv(files[-3])
 
 # IR
@@ -78,3 +92,65 @@ g23_catwise = pd.read_csv(files[3])
 # Radio
 g09_radio = pd.read_csv(files[2])
 g23_radio = pd.read_csv(files[1])
+
+#------------------------#
+# Diagnostic comparison
+#------------------------#
+# Optical Index
+Optical_index = np.zeros(len(g09_opt))
+Optical_index[(g09_opt.BPT_index==1)|(g09_opt.Blue_index==3)|(g09_opt.Blue_index==4)|(g09_opt.WHAN_index==3)|
+              (g09_opt.WHAN_index==4)|(g09_opt.MEx_index==3)|(g09_opt.CEx_index==2)] = 1
+g09_opt['Optical_index'] = Optical_index
+
+# IR Index
+g09_ir = g09_catwise.merge(g09_allwise[['component_id','mat_index']],on=['component_id'],how='inner')
+IR_index = np.zeros(len(g09_ir))
+IR_index[(g09_ir.assef_index==1)|(g09_ir.mat_index==1)] = 1
+g09_ir['IR_index'] = IR_index
+
+# X-ray combinations
+g09_opt_xray = g09_opt.merge(g09_Xray[['component_id','X_index']], on=['component_id'], how='inner')
+    #len(g09_opt_xray[(g09_opt_xray.Optical_index==1)&(g09_opt_xray.X_index==1)]) X-ray+opt
+    #g09_opt.Optical_index.value_counts().values[g09_opt.Optical_index.value_counts().keys()==1][0] Opt
+g09_ir_xray = g09_ir.merge(g09_Xray[['component_id','X_index']], on=['component_id'], how='inner') #0
+g09_radio_xray = g09_radio.merge(g09_Xray[['component_id','X_index']], on=['component_id'], how='inner') #0
+
+# Optical combinations
+g09_ir_opt = g09_ir.merge(g09_opt[['component_id','BPT_index','Blue_index','WHAN_index','MEx_index','CEx_index',
+                          'Optical_index']],on=['component_id'],how='inner')
+                          #len(g09_ir_opt[(g09_ir_opt.Optical_index==1)&(g09_ir_opt.IR_index==1)]) IR+opt
+g09_radio_opt = g09_radio.merge(g09_opt[['component_id','BPT_index','Blue_index','WHAN_index','MEx_index','CEx_index',
+                          'Optical_index']],on=['component_id'],how='inner')
+                          #len(g09_radio_opt[(g09_radio_opt.Optical_index==1)|(g09_radio_opt.radio_index==1)]) Radio+opt
+
+# IR combinations
+g09_radio_ir = g09_radio.merge(g09_ir[['component_id','assef_index','mat_index','IR_index']],on=['component_id'],
+                                how = 'inner')
+#len(g09_radio_ir[(g09_radio_ir.IR_index==1)&(g09_radio_ir.radio_index==1)])
+
+
+# Optical-IR-radio
+g09_opt_ir_radio = g09_ir_opt.merge(g09_radio[['component_id','radio_index']],on=['component_id'],how='inner')
+    # len(g09_opt_ir_radio[(g09_opt_ir_radio.Optical_index==1)&(g09_opt_ir_radio.IR_index==1)&(g09_opt_ir_radio.radio_index==1)])
+
+#------------#
+# UpSet plot
+#------------#
+data = from_memberships(
+    [
+        ('X-ray',),
+        ('Optical',),
+        ('IR',),
+        ('Radio',),
+        ('X-ray', 'Optical'),
+        ('Optical', 'IR'),
+        ('Optical','Radio'),
+        ('IR','Radio'),
+        ('Optical', 'IR', 'Radio')
+    ],
+    [324, 4598, 6136, 22807, 62, 373, 552, 2637, 47]
+)
+
+UpSet(data).plot()
+plt.yscale('log')
+plt.show()
